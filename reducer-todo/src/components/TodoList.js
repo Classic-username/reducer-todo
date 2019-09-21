5dff7d603dc58f228ac0f42bcaa9fda391ac72e8
Joshua Draper <josh.drapergame@gmail.com>
2019-09-20 22:02:09 -0600

    Incomplete. working on steps 4 and 5

--- reducer-todo/src/components/TodoList.js
+++ reducer-todo/src/components/TodoList.js
@@ -10,34 +10,38 @@ const TodoList = () => {
     const handleChanges = e => {
         setNewTodo(e.target.value);
     }
-    console.log(state)
+
+    const submitChanges = (e) => {
+        e.preventDefault();
+        dispatch({type: 'UPDATE_TODO',
+        payload: newTodo})
+    }
+
+    const toggleComplete = (e) => {
+        dispatch()
+    }
+
+    
+    console.log(state.item)
 
     return (
         <div>
             <h3>Welcome to your to-do list.</h3>
+            <form onSubmit={submitChanges}>
+                <div>
+                    <input 
+                        className='todo-input'
+                        type='text'
+                        value={newTodo}
+                        onChange={handleChanges}
+                    />
+                    <button type='submit'>Update list</button>
+                    <button onClick={toggleComplete}>Clear</button>
+                </div>
+            </form>
             <div>
-                <input 
-                    className='todo-input'
-                    type='text'
-                    name={newTodo}
-                    onChange={handleChanges}
-                />
-                <button
-                    onClick={ () => 
-                        dispatch({
-                            type: 'UPDATE_TODO', 
-                            payload: newTodo
-                        })
-                    }
-                >
-                    Update list
-                </button>
-            </div>
-            <div>
-                <p>{state.map(todo => {
-                    if(todo.completed === false){
+                <p>{state.list.map(todo => {
                         return <p id={todo.id}>{todo.item}</p>
-                    }
                 })}</p>
             </div>
         </div>
--- reducer-todo/src/reducers/reducers.js
+++ reducer-todo/src/reducers/reducers.js
@@ -1,11 +1,27 @@
-export const initialTodoState = [{
+export const initialTodoState = {
+    list: [
+    {
     item: 'Learn about reducers',
     completed: false,
     id: 3892987589
-}]
+    }]
+}
 
 export const todoReducer = (state, action) => {
     switch(action.type) {
+        case "UPDATE_TODO":
+            return {
+                list: [
+                    ...state.list, 
+                    {item: action.payload,
+                         completed: false, 
+                         id: new Date()}
+                ]
+            }
+            case 'CLEAR_TODO':
+                return{
+                    
+                }
         default:
             return state;
     }
